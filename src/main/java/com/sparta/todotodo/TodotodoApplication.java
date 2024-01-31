0c942dc2e499cb361839dcf7507a64f54cd67e1b
kudongku <kudongku@naver.com>
2024-01-31 20:52:51 +0900

    chore: init commit

---
+++ src/main/java/com/sparta/todotodo/TodotodoApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.todotodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodotodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodotodoApplication.class, args);
+    }
+
+}
