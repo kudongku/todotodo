f57184bd41adb0cf221210a0e7adb10fb9d6ca78
kudongku <kudongku@naver.com>
2024-02-01 19:56:15 +0900

    feat: index, login, signup으로 가는 get api 구현

---
+++ src/main/java/com/sparta/todotodo/user/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.sparta.todotodo.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/signup")
+    public String signupPage() {
+        return "signup";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+}
